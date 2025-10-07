d872d2a88bac516107c4effa768c2c90284eabef
Rishab <rishab.chandgothia.cg@gmail.com>
2025-10-07 11:46:03 +0530

    assignment-1

---
+++ 06.js
@@ -0,0 +1,16 @@
+var temp = 24;
+if(temo<0){
+    console.log("Freezing Cold");
+}
+else if(temp<=15){
+    console.log("Very Cold");
+}
+else if(temp<=25){                  
+    console.log("Cold");
+}
+else if(temp<=35){
+    console.log("Warm");
+}
+else{
+    console.log("Hot");
+}
